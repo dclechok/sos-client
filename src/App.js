26bdb0882ae10fbb8446ae259e8832b41a28b2b9
DC Lechok <dclechok@gmail.com>
2025-05-28 21:55:19 -0400

    building out dark color theme nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
 import './App.css';
+
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
+     <NavBar />
       <header className="App-header">test
       </header>
     </div>
---
+++ src/NavBar.js
@@ -0,0 +1,19 @@
+import './NavBar.css';
+
+function NavBar(){
+
+    return(
+        <div className="nav-bar">
+            <button className="nav-item">Home</button>
+            <button className="nav-item">Stats</button>
+            <button className="nav-item">Map</button>
+            <button className="nav-item">Inventory</button>
+            <button className="nav-item">Test4</button>
+            <button className="nav-item">Test5</button>
+            <button className="nav-item">Settings</button>
+        </div>
+    );
+
+}
+
+export default NavBar;
