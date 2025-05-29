5b3f94529f04312d43b94e036669a7e872f68516
DC Lechok <dclechok@gmail.com>
2025-05-28 22:29:09 -0400

    building our mud theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import NavBar from './NavBar';
+import MainText from './MainText';
 
 function App() {
   return (
     <div className="App">
      <NavBar />
+     <MainText />
       <header className="App-header">test
       </header>
     </div>
---
+++ src/MainText.js
@@ -0,0 +1,15 @@
+import './MainText.css';
+
+function MainText(){
+
+    return(
+        <div className="main-text-container">
+
+            <p>The rain hit the rusted rooftops like static against a dead channel. Neon signs flickered through the smog, casting warped reflections in puddles of oil and blood. Sirens howled somewhere beyond the barricades, their wails swallowed by the thrum of distant turbines and the hiss of steam vents bleeding from fractured infrastructure. In the alleys below, data runners huddled in shadows, masking their neural signatures from patrolling drones. The sky was a grid of surveillance satellites and glitching ad banners — hope had long since been overwritten. Tonight, something shifted in the code of the city. Someone new had entered the Sprawl… and the system was already watching...</p>
+            <p className="other-chat">Laeik: Get fucked!</p>
+
+        </div>
+    );
+}
+
+export default MainText;
