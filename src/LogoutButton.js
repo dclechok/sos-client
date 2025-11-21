c266b54404fe71da10dbd872ad3134893af44160
DC Lechok <dclechok@gmail.com>
2025-11-21 17:38:11 -0500

    dressing up logout warning/button

--- src/LogoutButton.js
+++ src/LogoutButton.js
@@ -1,22 +1,53 @@
 import "./styles/LogoutButton.css";
 import buttonClickSound from './sounds/button_click2.wav';
+import { useState } from 'react';
 
 function LogoutButton({ setAccount, setCharacter }) {
 
   const clickSound = new Audio(buttonClickSound);
+  const [warningMsg, setWarningMsg] = useState(false);
 
   function handleLogout() {
     localStorage.removeItem("pd_token");
+    localStorage.removeItem("pd_account");
+    localStorage.removeItem("pd_character");
+
     clickSound.currentTime = 0;
-    clickSound.play();
-    setAccount(null);  // forces return to Login screen
-    setCharacter(null); //forces character reselection on next login
+    clickSound.play().catch(() => {});
+
+    setAccount(null);
+    setCharacter(null);
+  }
+
+  function handleLogoutCheck() {
+    setWarningMsg(true);
+  }
+
+  function handleCancel() {
+    setWarningMsg(false);
   }
 
   return (
-    <button className="logout-btn" onClick={handleLogout}>
-      LOG OUT
-    </button>
+    <div className="logout-wrapper">
+
+      {/* LOGOUT BUTTON */}
+      <button className="logout-btn" onClick={handleLogoutCheck}>
+        LOG OUT
+      </button>
+
+      {/* CONFIRMATION BOX */}
+      {warningMsg && (
+        <div className="validate-logout">
+          <div>Are you sure you wish to logout here?</div>
+
+          <div className="logout-choice-buttons">
+            <button className="logout-yes" onClick={handleLogout}>Yes</button>
+            <button className="logout-no" onClick={handleCancel}>No</button>
+          </div>
+        </div>
+      )}
+
+    </div>
   );
 }
 
