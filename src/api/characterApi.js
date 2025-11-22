7864f0e0075e869d4f2966cb27e4a81859a908d0
DC Lechok <dclechok@gmail.com>
2025-11-21 21:07:55 -0500

    fixing the other underscore

--- src/api/characterApi.js
+++ src/api/characterApi.js
@@ -3,7 +3,7 @@ const BASE_URL = process.env.REACT_APP_API_BASE_URL + "/api/characters/";
 export async function fetchCharacterList(account, token) {
   console.log(account.id)
   try {
-    const response = await fetch(BASE_URL + account._id, {
+    const response = await fetch(BASE_URL + account.id, {
       method: "GET",
       headers: {
         "Authorization": `Bearer ${token}`
